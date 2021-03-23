d55c52c263bf54b34875ad150a31996ad65af760
Duncan <duncanmays@outlook.com>
2021-03-23 17:08:12 -0400

    ran another FLOPS test, changed data recording a little bit

---
+++ FLOPS_testing/plot_head.py
@@ -0,0 +1,16 @@
+import json
+from matplotlib import pyplot as plt
+
+f = open('./head.data', 'r')
+raw = f.read()
+f.close()
+
+data = json.loads(raw)
+
+x = [s['benchmark_score'] for s in data]
+y = [s['sample_rate'] for s in data]
+
+plt.scatter(x, y)
+plt.xlabel('benchmark score')
+plt.ylabel('training rate (samples per second)')
+plt.show()
